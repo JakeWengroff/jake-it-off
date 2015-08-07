faee8672e892a761bcc33efa009ffb5e8eb00892
Jake Wengroff <j@jxb1.com>
2015-08-06 22:11:25 -0400

    Fixed time zone discrepancy.

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Blocitoff
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
