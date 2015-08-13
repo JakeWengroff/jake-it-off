b1fd0ee55ebc5eefcf7f7a7535fe8bec04c7d7cf
Jake Wengroff <j@jxb1.com>
2015-08-12 23:33:24 -0400

    Created new landing page.
    Created new home page; changed Routes; amended ApplicationController to create a redirect after signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-    
+
   get 'welcome/index'
   get 'welcome/about'
-  root to: 'users#show'
+  root to: 'welcome#index'
 
   resources :users, only: [:show] do
     resources :items, only: [:create, :destroy]
