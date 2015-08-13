ae3d49f662be04c08985d7f07ff6160692d51080
Jake Wengroff <j@jxb1.com>
2015-08-13 00:22:55 -0400

    Fixed UsersController spec file.
    Tests are all passing now.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,19 +5,15 @@ RSpec.describe UsersController, type: :controller do
   before(:each) do
     @user = FactoryGirl.create(:user)
     sign_in @user
+    @item = FactoryGirl.create(:item)
+    @user.items << @item
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, :id => @user.id
       expect(response).to have_http_status(:success)
-    end 
+    end
   end
 
 end
-
-
- 
-
-
-
